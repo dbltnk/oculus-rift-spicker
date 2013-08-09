5ed872a6e4e430d74fe3f2574d1b87fb55787b76
Alexander Zacherl <alexander.zacherl@googlemail.com>
2013-08-09 19:07:15 +0200

    added random footstps

--- Assets/Scripts/Teacher.cs
+++ Assets/Scripts/Teacher.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
+[RequireComponent(typeof(AudioSource))]
 public class Teacher : MonoBehaviour {
 
+	public List<AudioClip> footsteps;
+	
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +14,14 @@ public class Teacher : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		PlayRandomSFX();
 	}
+	
+	void PlayRandomSFX(){
+		if (audio.isPlaying) return; 
+		else {
+			audio.clip = RandomHelper.pickRandom(footsteps);
+			audio.Play();
+			}
+		}
 }
