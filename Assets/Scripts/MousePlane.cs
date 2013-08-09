be92aee18e4e5ee23881e5ddc781cc66fc72f65c
Sebastian Dorda <sd@bitbarons.com>
2013-08-09 18:25:14 +0200

    player setup and mouse plane

---
+++ Assets/Scripts/MousePlane.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MousePlane : MonoBehaviour {
+	
+	public float _width;
+	public float _height;
+	
+	void OnDrawGizmosSelected() {
+		Gizmos.color = Color.magenta;
+		
+		var w2 = _width / 2f;
+		var h2 = _height / 2f;
+		
+		var p0 = transform.TransformPoint(new Vector3(-w2,0f,h2));
+		var p1 = transform.TransformPoint(new Vector3(w2,0f,h2));
+		var p2 = transform.TransformPoint(new Vector3(w2,0f,-h2));
+		var p3 = transform.TransformPoint(new Vector3(-w2,0f,-h2));
+		
+		Gizmos.DrawLine(p0, p1);
+		Gizmos.DrawLine(p1, p2);
+		Gizmos.DrawLine(p2, p3);
+		Gizmos.DrawLine(p3, p0);
+	}
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	
+	public Paper _paper;
+	public Hand _hand;
+	public MousePlane _mousePlane;
+	
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
