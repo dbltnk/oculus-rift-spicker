eb6782f889920420e13c3f2ac4bcc6edcbc91351
Sebastian Dorda <sd@bitbarons.com>
2013-08-09 17:32:28 +0200

    bye bye tuscany

--- Assets/Scripts/Paper.cs
+++ Assets/Scripts/Paper.cs
@@ -4,6 +4,12 @@ using System.Collections.Generic;
 
 public class Paper : MonoBehaviour {
 	
+	public static int[] correctAnswers = {
+		0, 1, 1, 2, 0, 2,
+	};
+	
+	public static float percentageCorrect = 0.75f;
+	
 	public static Vector2[] textureOffsets = {
 		new Vector2(0.5f, 0.5f),	// red
 		new Vector2(0f, 0f),		// green
@@ -14,22 +20,61 @@ public class Paper : MonoBehaviour {
 	public List<GameObject> _answers0;
 	public List<GameObject> _answers1;
 	public List<GameObject> _answers2;
+	public List<GameObject> _answers3;
+	public List<GameObject> _answers4;
+	public List<GameObject> _answers5;
 	
-	private void EnableOneRandom(List<GameObject> l)
+	public bool _isPrefilled;
+	
+	private void EnableOneRandom(List<GameObject> l, int correctAnswer)
 	{
-		int r = RandomHelper.next(0, l.Count-1);
+		int answer = correctAnswer;
+		
+		// wrong?
+		if (RandomHelper.next() > percentageCorrect)
+		{
+			// pick wrong random
+			while (answer == correctAnswer)
+			{
+				answer = RandomHelper.next(0, l.Count-1);
+			}
+		}
+		
+		// set
 		for(int i = 0; i < l.Count; ++i)
 		{
-			l[i].SetActive(i == r);
+			l[i].SetActive(i == answer);
+		}
+	}
+	
+	private void PrepareAnswers(List<GameObject> l)
+	{
+		// set
+		for(int i = 0; i < l.Count; ++i)
+		{
+			l[i].SetActive(false);
 			l[i].renderer.material.mainTextureOffset = textureOffsets[i];
 		}
 	}
 	
 	// Use this for initialization
 	void Start () {
-		EnableOneRandom(_answers0);
-		EnableOneRandom(_answers1);
-		EnableOneRandom(_answers2);
+		PrepareAnswers(_answers0);
+		PrepareAnswers(_answers1);
+		PrepareAnswers(_answers2);
+		PrepareAnswers(_answers3);
+		PrepareAnswers(_answers4);
+		PrepareAnswers(_answers5);
+			
+		if (_isPrefilled)
+		{
+			EnableOneRandom(_answers0, correctAnswers[0]);
+			EnableOneRandom(_answers1, correctAnswers[1]);
+			EnableOneRandom(_answers2, correctAnswers[2]);
+			EnableOneRandom(_answers3, correctAnswers[3]);
+			EnableOneRandom(_answers4, correctAnswers[4]);
+			EnableOneRandom(_answers5, correctAnswers[5]);
+		}
 	}
 	
 	// Update is called once per frame
